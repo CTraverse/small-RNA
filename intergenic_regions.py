487f6fef97bccae870b79f39701bd0e851b67bd5
CTraverse <32548575+CTraverse@users.noreply.github.com>
2018-10-31 08:49:58 -0400

    Update intergenic_regions.py

--- intergenic_regions.py
+++ intergenic_regions.py
@@ -4,47 +4,53 @@ Created on Thu Nov 16 15:26:45 2017
 
 @author: chuck
 """
+
+
+"""
+This script will loop through all genes in the genome and find the regions between the genes, termed 'intergenic regions'.
+It builds in an extra 100 bases on either end of the intergenic region for analysis of the surrounding sequence
+"""
+
 from Bio import SeqIO
+
+#import the genome file with gene coordinates
 genome = list(SeqIO.parse('/home/chuck/Documents/RNAseq/REL606_2.gff', 'fasta'))
 
+#open the file that the intergenic regions will be written to
 intergenic_write = open('/home/chuck/Documents/RNAseq/REL606_genic_and_intergenic_3.txt', 'w')
 
+#Initialize previous end because there will not have been a previous gene at the start of the loop
 previous_end = genome[0].description.split('location=')[1].strip(']\n').split('..')[1]
 
-intergenic_list = []
-intergenic_dict = {}
+#Delete the first gene in the genome since previous end is already initialized
+##Start the actual loop on the second gene
 del genome[0]
-i = 0
 intergenic_count = 0
 gene_count = 0
+
+#Loop through each gene in the genome and find the regions between each consecutive gene
 for gene in genome:
-    if 'complement' not in gene.description:
+    if 'complement' not in gene.description: #Determine if the gene is on the forward or reverse strand
         locations = gene.description.split('location=')[1].strip(']\n')
         
     else:
         locations = gene.description.split('complement(')[1].strip(')]\n')
-
+    
+    #Counter for which gene we're on
     gene_count += 1
+    #What is the coordinate designating the start of the current gene in the loop?
     current_start = locations.split('..')[0].strip('<').strip('>')
-    dist = int(current_start)-int(previous_end)    
-    intergenic_list.append(int(current_start)-int(previous_end))
-    if dist not in intergenic_dict:
-        intergenic_dict[dist] = 1
-    else:
-        intergenic_dict[dist] += 1
-        
+
+    #Only take the intergenic regions that are greater than 100 bases in length
+    #Then write the locations if each gene and each intergenic region in order
     if int(current_start) - int(previous_end) > 100:
         intergenic_count += 1
-        i += 1
         intergenic_write.write("%s\t%s\t%s\n" % ("intergenic_" + str(intergenic_count), str(int(previous_end)+25), str(int(current_start)-25)))
+        
     intergenic_write.write("%s\t%s\t%s\n" % ("gene_" + str(gene_count), current_start, locations.split('..')[1].strip('<').strip('>')))
-
+    
+    #update the previous end to be used in the next iteration
     previous_end = locations.split('..')[1].strip('<').strip('>')
 
-new_list = sorted(list(set(intergenic_list)))
-
-for item in new_list:
-    print item, intergenic_dict[item]
-print i
 
 intergenic_write.close()
